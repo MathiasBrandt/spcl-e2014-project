6a6ee82b047226cab2b9cf0eecc6a4bca55c29d2
Mathias Brandt <maini123@gmail.com>
2014-09-27 03:01:19 +0200

    Completed the create user flow.

--- android/SPCLSmartphoneApp/app/src/main/java/dk/itu/group10/spclsmartphoneapp/LoginActivity.java
+++ android/SPCLSmartphoneApp/app/src/main/java/dk/itu/group10/spclsmartphoneapp/LoginActivity.java
@@ -1,6 +1,7 @@
 package dk.itu.group10.spclsmartphoneapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,7 +10,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
@@ -19,7 +24,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
 
@@ -38,7 +43,7 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // DEBUGGING
+        // TODO: DEBUGGING REMOVE THIS
         saveUserId(PREFS_DEFAULT_USER_ID);
 
         // check preferences for user id
@@ -54,6 +59,10 @@ public class LoginActivity extends Activity {
         txtEmail = (EditText) findViewById(R.id.txtEmail);
     }
 
+    /***
+     * Retrieves the user id stored in SharedPreferences.
+     * @return the stored user id or -1 if no user is exists in SharedPreferences.
+     */
     private int getStoredUserId() {
         SharedPreferences preferences = getPreferences(MODE_PRIVATE);
         int userId = preferences.getInt(PREFS_USER_ID_KEY, PREFS_DEFAULT_USER_ID);
@@ -67,29 +76,44 @@ public class LoginActivity extends Activity {
         return userId;
     }
 
+    /***
+     * Saves a user id to SharedPreferences.
+     * @param userId the user id to save.
+     */
     private void saveUserId(int userId) {
         SharedPreferences preferences = getPreferences(MODE_PRIVATE);
 
         Log.d(TAG, String.format("Saving new user id to preferences: %d", userId));
 
-        preferences.edit().putInt(PREFS_USER_ID_KEY, userId).commit();
+        preferences.edit().putInt(PREFS_USER_ID_KEY, userId).apply();
     }
 
+    /***
+     * Navigates the user interface to MainActivity.
+     */
     private void navigateToMainActivity() {
         Intent i = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(i);
     }
 
+    /***
+     * Click handler for the "Create User" button.
+     * @param v
+     */
     public void btnCreateUserOnClick(View v) {
         new CreateUserAsyncTask().execute();
     }
 
+    /***
+     * This class represents an async task that can be used to create a user
+     * on the server.
+     */
     class CreateUserAsyncTask extends AsyncTask<Void, Void, Integer> {
         private static final String NAME_KEY = "Name";
         private static final String PHONE_KEY = "Phone";
         private static final String EMAIL_KEY = "Email";
 
-        private ProgressDialog dialog = new ProgressDialog(LoginActivity.this);
+        private ProgressDialog loadingDialog = new ProgressDialog(LoginActivity.this);
         private String name;
         private String phone;
         private String email;
@@ -98,12 +122,63 @@ public class LoginActivity extends Activity {
         protected Integer doInBackground(Void... voids) {
             // TODO: validate input fields
 
-            int userId = -1;
+            return postUser();
+        }
 
-            // create a user and get the user id
-            userId = createUser();
+        /***
+         * Tries to create a user on the server by sending a POST request.
+         * @return the id of the newly created user or -1 on failure.
+         */
+        private int postUser() {
+            HttpClient client = new DefaultHttpClient();
+            HttpPost postRequest = new HttpPost(API_CREATE_USER_URL);
+            postRequest.addHeader("content-type", "application/json");
+            HttpResponse response = null;
 
-            return userId;
+            User user = new User(name, phone, email, "1");
+            String userJson = serializeUser(user);
+
+            StringEntity params = null;
+
+            try {
+                params = new StringEntity(userJson);
+            } catch (UnsupportedEncodingException e) {
+                Log.d(TAG, "UnsupportedEncodingException");
+            }
+
+            postRequest.setEntity(params);
+
+            try {
+                Log.d(TAG, String.format("Creating new user: %s", userJson));
+
+                response = client.execute(postRequest);
+            } catch (ClientProtocolException e) {
+                Log.d(TAG, "ClientProtocolException");
+            } catch (IOException e) {
+                Log.d(TAG, "IOException");
+            }
+
+            Log.d(TAG, "Server returned status code: " + response.getStatusLine().getStatusCode());
+
+            String responseString = "";
+
+            if(response != null && response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                // read the response (the user id)
+                try {
+                    InputStream is = response.getEntity().getContent();
+                    responseString = IOUtils.toString(is);
+
+                    Log.d(TAG, "Full response: " + responseString);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                user = deserializeUser(responseString);
+                return user.getId();
+            }
+
+            Log.d(TAG, "Could not create user");
+            return PREFS_DEFAULT_USER_ID;
         }
 
         @Override
@@ -111,16 +186,14 @@ public class LoginActivity extends Activity {
             super.onPreExecute();
 
             // show loading dialog
-            dialog.setTitle("Creating User");
-            dialog.setMessage("Please wait ...");
-            dialog.show();
+            loadingDialog.setTitle(R.string.create_user_loading_title);
+            loadingDialog.setMessage(getApplicationContext().getString(R.string.create_user_loading_message));
+            loadingDialog.show();
 
             // fetch variables from text fields
             name = txtName.getText().toString().trim();
             phone = txtPhone.getText().toString().trim();
-            email = txtEmail.getText().toString().trim();
-
-            Log.d(TAG, String.format("Creating new user: %s, %s, %s", name, phone, email));
+            email = txtEmail.getText().toString().trim().toLowerCase();
         }
 
         @Override
@@ -131,52 +204,44 @@ public class LoginActivity extends Activity {
             saveUserId(userId);
 
             // dismiss the loading dialog
-            dialog.dismiss();
+            loadingDialog.dismiss();
 
-            // navigate to main activity
-            navigateToMainActivity();
+            if(userId != PREFS_DEFAULT_USER_ID) {
+                // show success message
+                Toast.makeText(LoginActivity.this, getApplication().getString(R.string.create_user_success_message), Toast.LENGTH_SHORT).show();
+
+                // navigate to main activity
+                navigateToMainActivity();
+            } else {
+                // create an alert dialog showing an error message
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                builder.setTitle(R.string.create_user_error_title)
+                        .setMessage(getApplicationContext().getString(R.string.create_user_error_message))
+                        .setPositiveButton("OK", null);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
         }
 
-        private int createUser() {
-            HttpClient client = new DefaultHttpClient();
-            HttpPost postRequest = new HttpPost(API_CREATE_USER_URL);
-            HttpResponse response = null;
+        /***
+         * Serializes a user into JSON.
+         * @param user the user object to serialize.
+         * @return a JSON string representing the user.
+         */
+        private String serializeUser(User user) {
+            Gson gson = new Gson();
+            return gson.toJson(user);
+        }
 
-            User user = new User(name, phone, email, "1");
-            StringEntity params = null;
-            try {
-                params = new StringEntity(user.getJson());
-            } catch (UnsupportedEncodingException e) {
-                Log.d(TAG, "UnsupportedEncodingException");
-            }
-            postRequest.setEntity(params);
-            postRequest.addHeader("content-type", "application/json");
-
-            try {
-                response = client.execute(postRequest);
-            } catch (ClientProtocolException e) {
-                Log.d(TAG, "ClientProtocolException");
-            } catch (IOException e) {
-                Log.d(TAG, "IOException");
-            }
-
-            Log.d(TAG, "Status code: " + response.getStatusLine().getStatusCode());
-
-            if(response != null && response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                // read the response (the user id)
-                try {
-                    InputStreamReader isr = new InputStreamReader(response.getEntity().getContent());
-                    char[] buffer = new char[512];
-                    isr.read(buffer);
-                    Log.d(TAG, new String(buffer));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-            Log.d(TAG, "Could not create user");
-            return -1;
+        /***
+         * Deserializes a JSON string into a user object.
+         * @param json the JSON string to deserialize.
+         * @return a user object representing the JSON string.
+         */
+        private User deserializeUser(String json) {
+            Gson gson = new Gson();
+            return gson.fromJson(json, User.class);
         }
     }
 }
--- android/SPCLSmartphoneApp/app/src/main/java/dk/itu/group10/spclsmartphoneapp/User.java
+++ android/SPCLSmartphoneApp/app/src/main/java/dk/itu/group10/spclsmartphoneapp/User.java
@@ -1,14 +1,22 @@
 package dk.itu.group10.spclsmartphoneapp;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by brandt on 27/09/14.
  */
 
 public class User {
-    String name;
-    String phone;
-    String email;
-    String statusId;
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+    @SerializedName("status_id")
+    private String statusId;
+
+    public User() {
+
+    }
 
     public User(String name, String phone, String email, String statusId) {
         this.name = name;
@@ -17,15 +25,7 @@ public class User {
         this.statusId = statusId;
     }
 
-    public String getJson() {
-        String s = "";
-        s += "{";
-        s += "\"name\":\"" + name + "\",";
-        s += "\"phone\":\"" + phone + "\",";
-        s += "\"email\":\"" + email + "\",";
-        s += "\"status_id\":\"" + statusId + "\"";
-        s += "}";
-
-        return s;
+    public int getId() {
+        return id;
     }
 }
