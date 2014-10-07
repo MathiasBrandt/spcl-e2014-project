b1c28b2752ee3157720f9c4928d38d2c08b9f596
Mathias Brandt <maini123@gmail.com>
2014-10-07 19:27:45 +0200

    Added Main, SelectUser activities to tablet project

---
+++ android/Pervasive/tablet/src/main/java/dk/itu/pervasive/tablet/activities/SelectUserActivity.java
@@ -0,0 +1,15 @@
+package dk.itu.pervasive.tablet.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import dk.itu.pervasive.tablet.R;
+
+public class SelectUserActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_user);
+    }
+}
