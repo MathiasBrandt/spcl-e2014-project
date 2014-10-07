917740b3d487fbf8efbdbc549b2a3dfcde0401d8
Mathias Brandt <maini123@gmail.com>
2014-10-08 00:51:46 +0200

    MainActivity now sets color according to user's state, has logout button

--- android/Pervasive/tablet/src/main/java/dk/itu/pervasive/tablet/activities/MainActivity.java
+++ android/Pervasive/tablet/src/main/java/dk/itu/pervasive/tablet/activities/MainActivity.java
@@ -1,7 +1,7 @@
 package dk.itu.pervasive.tablet.activities;
 
+import android.app.ActionBar;
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,10 +13,9 @@ import dk.itu.pervasive.common.User;
 import dk.itu.pervasive.tablet.R;
 
 public class MainActivity extends Activity {
+    public static final String TAG = "MainActivity";
     RelativeLayout userStateFrame;
     TextView userStateText;
-
-    private Boolean derp = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +24,38 @@ public class MainActivity extends Activity {
 
         userStateFrame = (RelativeLayout) findViewById(R.id.frameUserState);
         userStateText = (TextView) findViewById(R.id.txtUserState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         User user = Common.getUserFromPreferences(this);
 
         if(user == null) {
             // navigate to SelectUserActivity
             Common.navigateToActivity(this, SelectUserActivity.class);
+        } else {
+            setUserName(user.getName());
+            setUserState(user.getStatusId());
         }
     }
 
-    private void setState(int state) {
+    private void setUserName(String userName) {
+        TextView txtUserName = (TextView) findViewById(R.id.txtUserName);
+        txtUserName.setText(String.format("%s is", userName));
+    }
+
+    private void setUserState(int state) {
+        ActionBar actionBar = getActionBar();
+
         switch(state) {
             case Common.USER_STATE_AVAILABLE:
-                userStateFrame.setBackgroundColor(Color.GREEN);
+                userStateFrame.setBackgroundColor(R.color.state_color_green);
                 userStateText.setText(R.string.state_available);
                 break;
             case Common.USER_STATE_BUSY:
-                userStateFrame.setBackgroundColor(Color.RED);
+                userStateFrame.setBackgroundColor(R.color.state_color_red);
                 userStateText.setText(R.string.state_busy);
                 break;
         }
@@ -56,13 +70,13 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_item_logout) {
+            Common.removeUserInPreferences(this);
+            Common.navigateToActivity(this, SelectUserActivity.class);
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
