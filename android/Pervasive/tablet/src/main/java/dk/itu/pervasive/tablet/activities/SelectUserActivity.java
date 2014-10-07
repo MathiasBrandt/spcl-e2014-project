8d85e3c422e4299604bbbd173af80beab6e2d0c2
Mathias Brandt <maini123@gmail.com>
2014-10-08 00:52:32 +0200

    Implemented SelectUserActivity with dummy data.

--- android/Pervasive/tablet/src/main/java/dk/itu/pervasive/tablet/activities/SelectUserActivity.java
+++ android/Pervasive/tablet/src/main/java/dk/itu/pervasive/tablet/activities/SelectUserActivity.java
@@ -2,14 +2,44 @@ package dk.itu.pervasive.tablet.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
+import dk.itu.pervasive.common.Common;
+import dk.itu.pervasive.common.User;
 import dk.itu.pervasive.tablet.R;
 
 public class SelectUserActivity extends Activity {
+    public static final String TAG = "SelectUserActivity";
+    ListView lstUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_user);
+
+        lstUsers = (ListView) findViewById(R.id.lstUsers);
+        lstUsers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                User user = (User) parent.getItemAtPosition(position);
+                Common.saveUserToPreferences(SelectUserActivity.this, user);
+                SelectUserActivity.this.finish();
+            }
+        });
+
+        populateUserList();
+    }
+
+    private void populateUserList() {
+        User user1 = new User("Mathias", "12345678", "e@mail.dk", 1);
+        User user2 = new User("Rasmus", "87654321", "mail@e.dk", 2);
+        User[] users = new User[] { user1, user2 };
+
+        ListAdapter adapter = new ArrayAdapter<User>(this, android.R.layout.simple_list_item_1, users);
+        lstUsers.setAdapter(adapter);
     }
 }
